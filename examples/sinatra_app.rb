81a9904a54a44f64f9492cc9b6b3a5210223b4a1
julien <julien.genestoux@gmail.com>
2012-01-19 22:20:04 -0500

    docs improved, example clarified

--- examples/sinatra_app.rb
+++ examples/sinatra_app.rb
@@ -3,13 +3,10 @@ require 'rack-superfeedr'
 
 use Rack::Superfeedr, { :host => "plant-leg.showoff.io", :login => "demo", :password => "demo", :format => "json", :async => false } do |superfeedr|
   Superfeedr = superfeedr
-  superfeedr.on_notification do |notification|
-    puts notification.to_s
-  end
 end
 
 get '/hi' do
-  "Hello World!"
+  "Hello World!" # Maybe serve the data you saved from Superfeedr's handler.
 end
 
 get '/subscribe' do
@@ -18,4 +15,8 @@ end
 
 get '/unsubscribe' do
   Superfeedr.unsubscribe("http://push-pub.appspot.com/feed", 123)
+end
+
+Superfeedr.on_notification do |notification|
+  puts notification.to_s # You probably want to persist that data in some kind of data store...
 end
